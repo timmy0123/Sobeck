b04d91d8675a9cdc04905aef2f522ffd99fc0cab
timmy0123 <48618505+timmy0123@users.noreply.github.com>
2020-08-02 23:29:28 +0800

    Add files via upload

--- sobek_analysis.py
+++ sobek_analysis.py
@@ -7,11 +7,12 @@ import export_file
 input_folder = input("請輸入資料夾位置")
 year = input("請輸入開始年分")
 month = input("請輸入開始月分")
+day = input("請輸入開始日期") 
 hour = input("請輸入幾點開始")
 minute = input("請輸入幾分開始")
 os.chdir(input_folder)
 
-year,month,hour,minute = int(year),int(month),int(hour),int(minute)
+year,month,day,hour,minute = int(year),int(month),int(day),int(hour),int(minute)
 
 result_path = os.getcwd() + "/Result/"
 video_path = result_path + "out/"
@@ -54,5 +55,5 @@ for i in range(1,434):
 
   print("讀取%s"%filename)
 
-time = time_bar_create.time_bar()
+time = time_bar_create.time_bar(year,month,day,hour,minute)
 out = export_file.output_file(result_path,video_path,word_path,max_index,area_index,time,map)
